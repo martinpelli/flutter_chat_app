3f4b638a059ec725218faddeaee6bf5aecfe6af6
Martin Pellicer <martinpelli@outlook.com>
2022-11-28 12:17:10 -0300

    Initial commit

---
+++ lib/routes/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/screens/chat_screen.dart';
+import 'package:flutter_chat_app/screens/loading_screen.dart';
+import 'package:flutter_chat_app/screens/login_screen.dart';
+import 'package:flutter_chat_app/screens/register_screen.dart';
+import 'package:flutter_chat_app/screens/users_screen.dart';
+
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+  'usuarios': (_) => UsersScreen(),
+  'chat': (_) => ChatScreen(),
+  'login': (_) => LoginScreen(),
+  'register': (_) => RegisterScreen(),
+  'loading': (_) => LoadingScreen(),
+};
